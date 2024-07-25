9c8e0a0843d5082c0ccc210a4c6c5bed962f8b24
Ojas Sharma <23020583@iclms.ac.uk>
2024-07-25 17:49:32 +0530

    Added license stuff to code.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+''' Smart Intersections - Simulation of human-driven and autonomous vehicles in a city environment.
+    Copyright (C) 2024 Ojas Sharma
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 # LIBRARIES
 
 import math
@@ -32,6 +49,8 @@ amax = 3
 stepsPerSecond = 50
 
 # CODE
+
+print("\nSmart Intersections - Simulation of human-driven and autonomous vehicles in a city environment.\nCopyright (C) 2024 Ojas Sharma.\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under certain conditions.\n")
 
 map = RoadNetwork('map1')
 map.plotMapGraph()
