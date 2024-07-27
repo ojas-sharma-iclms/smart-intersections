8d5c96ae5b650007ffea058e985170e6e9dda049
Ojas Sharma <23020583@iclms.ac.uk>
2024-07-27 11:25:54 +0530

    actually added turning detection :)

--- vehicles.py
+++ vehicles.py
@@ -23,6 +23,7 @@ import matplotlib.pyplot as plt
 import pygame, sys
 from statistics import stdev
 from random import randint
+import numpy as np
 import csv
 
 # CLASS DEFINITIONS
@@ -48,6 +49,7 @@ class Vehicle(RoadObject):
         self.amax = amax
         self.hst = hst
         self.hgo = hgo
+        self.turning = [] # whether the vehicle will need to turn at any intersections en-route
         self.vehicle_length = vehicle_length
         self.c = c
         self.human_reaction = human_reaction
@@ -56,12 +58,32 @@ class Vehicle(RoadObject):
         self.next = None
         RoadObject.__init__(self, id, roadMap, roadSection, positionInSection)
 
+        self.getOptimalRoute() # check if turning at any intersections en-route (code run only at beginning)
+        pathIds = []
+        for object in self.path:
+            pathIds.append(object.split(".", 1)[0])
+        counter = 0
+        for id in pathIds:
+            if (pathIds.count(id) == 3) and (id[0] == "I"):
+                if self.path[np.where(np.array(pathIds)==id)[0][1]] not in self.turning: self.turning.append(self.path[np.where(np.array(pathIds)==id)[0][1]])
+            if (pathIds.count(id) == 1) and (id[0] == "I"):
+                self.turning.append(self.path[counter])
+            counter += 1
+
     def getOptimalRoute(self):
         self.path = self.roadMap.getOptimalRoute(self.roadSection, self.destination)
         nextId = self.path[1].split(".", 1)[0]
         direction = self.path[1].split(".", 1)[1]
         self.next = self.roadMap.lookUp(nextId)
         return self.path
+
+    def getTurns(self):
+        pathIds = []
+        for object in self.path:
+            pathIds.append(object.split(".", 1)[0])
+        for id in pathIds:
+            if pathIds.count(id) == 3:
+                if id not in self.turning: self.turning.append(id)
     
     def getPath(self):
         self.next_object = self.roadMap.getPath(self.id, self.roadSection, self.destination)
